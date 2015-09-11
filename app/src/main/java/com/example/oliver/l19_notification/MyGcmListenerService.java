a22ca648ef6e4e4e39c8e00293a8e8ed10751386
Oleg Zolotar <zolotar1123@gmail.com>
2015-09-11 13:46:57 +0300

    Create Project

---
+++ app/src/main/java/com/example/oliver/l19_notification/MyGcmListenerService.java
@@ -0,0 +1,9 @@
+package com.example.oliver.l19_notification;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by oliver on 11.09.15.
+ */
+public class MyGcmListenerService extends GcmListenerService {
+}
---
+++ app/src/main/java/com/example/oliver/l19_notification/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.example.oliver.l19_notification;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by oliver on 11.09.15.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+}
